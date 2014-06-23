b1c58392e17b8ae7a006b5ba4819f043ca315be1
Korbinian Schweiger <korbinian.schweiger@student.kit.edu>
2014-06-23 22:02:59 +0200

    added possibility to look at more than one ptcut

--- extracthistos_2.py
+++ extracthistos_2.py
@@ -22,14 +22,16 @@ class Histograms(object):
         elif weight < 0:
             self.neg.Fill(value,-1.)
             self.combined.Fill(value,-1.)
-
     def write(self):
         outputfile.WriteTObject(self.pos)
         outputfile.WriteTObject(self.neg)
         outputfile.WriteTObject(self.combined)
+        del self
+
 #-------------------------------------------------#
 #---------------- Cut definitions ----------------#
-ptcut = 25 
+# ptcuts is a list of different cut values
+ptcuts = [25.,50.,100.] 
 etacut = 2.5
 #-------------------------------------------------#
 #First use FW Lite from CMSSW
@@ -42,117 +44,98 @@ ROOT.AutoLibraryLoader.enable()
 gSystem.Load("libDataFormatsFWLite.so")
 gSystem.Load("libDataFormatsPatCandidates.so")
 #-------------------------------------------------#
-#Check number of system arguments
-if len(sys.argv) == 3:
+#Create output file bevor loop
+outputfile = TFile(sys.argv[1],"RECREATE")
+
+for cut in ptcuts:
+    #Check number of system arguments
+    #Read in the inputfiles for every loop, else it wont work
+    if len(sys.argv) == 3:
 	events = Events (sys.argv[2])
-	outputfile = TFile(sys.argv[1],"RECREATE")
-elif len(sys.argv) == 4:
+    elif len(sys.argv) == 4:
 	events = Events ([sys.argv[2],sys.argv[3]])
-	outputfile = TFile(sys.argv[1],"RECREATE")
-elif len(sys.argv) == 12:
+    elif len(sys.argv) == 12:
 	events = Events ([sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],sys.argv[9],sys.argv[10],sys.argv[11]])
-	outputfile = TFile(sys.argv[1],"RECREATE")
-elif len(sys.argv) == 22:
+    elif len(sys.argv) == 22:
 	events = Events ([sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7],sys.argv[8],sys.argv[9],sys.argv[10],sys.argv[11],sys.argv[12],sys.argv[13],sys.argv[14],sys.argv[15],sys.argv[16],sys.argv[17],sys.argv[18],sys.argv[19],sys.argv[20],sys.argv[21]])
-	outputfile = TFile(sys.argv[1],"RECREATE")
-else:
+    else:
 	print "Something is wrong with the number of arguments!"
 	print "Use: python extracthistos.py output.root input.root, if you just want to extract the Histograms from a single root file"
 	print "Or use: python extracthistos.py output.root input1.root input2.root, if you want also to add the input Histograms"
 	print "currently inplemented: 10 inputs and 20 inputs"
 	exit()
-
-#Definition of the Histograms
-#Jet Histograms
-njets= Histograms ("HnJets", "Number of Jets", 15, -0.5, 14.5)
-pt = Histograms("Hpt","Gen-Jet pt",100,0,300)
-phi = Histograms("Hphi","Gen-Jet Phi",50,-pi,pi)
-theta = Histograms("Htheta","Gen-Jet Theta",50,0,pi)
-energy = Histograms("Henergy","Gen-Jet Energy",100,0,600)
-firstjetpt = Histograms("HfirstJetpt","Pt of hardest Gen-Jet", 100,0,300)
-firstjeteta = Histograms("H1sJeteta","Eta of hardest Gen-Jet",50,-5,5)
-secoundjetpt = Histograms("HsecoundJetpt","Pt of 2nd hardest Gen-Jet", 100,0,300)
-"""
-#Particle Histograms
-Htpt = TH1F ("Htpt","pt of Top-Quarks", 100,0,300)
-Htbarpt = TH1F ("Htbarpt","pt of Top-Anti-Quark", 100, 0, 300)
-Httbarpt = TH1F ("Httbarpt", "pt of t tbar pair", 100, 0 ,300)
-"""
-# create handle outside of loop
-# Handle and lable are pointing at the Branch you want
-handle  = Handle ('std::vector<reco::GenJet>')
-label = ("ak5GenJets")
-particlehandle = Handle ('std::vector<reco::GenParticle>')
-particlelabel = ("genParticles")
-infohandle = Handle ('<GenEventInfoProduct>')
+        
+    cutn = str(cut) #variable for names of histograms
+    #Definition of the Histograms
+    #Jet Histograms
+    njets = Histograms ("HnJets"+cutn, "Number of Jets "+cutn, 15, -0.5, 14.5)
+    pt = Histograms("Hpt"+cutn,"Gen-Jet pt "+cutn,100,0,300)
+    phi = Histograms("Hphi"+cutn,"Gen-Jet Phi "+cutn,50,-pi,pi)
+    theta = Histograms("Htheta"+cutn,"Gen-Jet Theta "+cutn,50,0,pi)
+    energy = Histograms("Henergy"+cutn,"Gen-Jet Energy "+cutn,100,0,600)
+    firstjetpt = Histograms("HfirstJetpt"+cutn,"Pt of hardest Gen-Jet "+cutn, 100,0,300)
+    firstjeteta = Histograms("H1sJeteta"+cutn,"Eta of hardest Gen-Jet "+cutn,50,-5,5)
+    secoundjetpt = Histograms("HsecoundJetpt"+cutn,"Pt of 2nd hardest Gen-Jet "+cutn, 100,0,300)
 
 
-#ROOT.gROOT.SetStyle('Plain') # white background
-#Loop through all Events and Fill the Histograms
-print 'Filling new jet histograms'
-enumber = 0
-for event in events:
-        #print "In event ", event
-	event.getByLabel (label, handle)
-	GenJets = handle.product()
-	ievent = event
-	ievent.getByLabel ("generator", infohandle)
-	Infos = infohandle.product()
-	nJets = 0
-	firstJetpt = 0
-	secoundJetpt = 0
-	firstJeteta = 0
-	eventweight = Infos.weight()
-	for Jet in GenJets:
-		if Jet.pt() >= ptcut and abs(Jet.eta()) <= etacut:
-                        #for JetConstituent in Jet.getJetConstituents():
-                        #        print JetConstituent.pdgId(), " no of mothers ", JetConstituent.numberOfMothers()
-			nJets = nJets + 1
-                        pt.fill(eventweight,Jet.pt())
-			phi.fill(eventweight,Jet.phi())
-			theta.fill(eventweight,Jet.theta())
-			energy.fill(eventweight,Jet.energy())
-			if Jet.pt() >= firstJetpt and Jet.pt() >= secoundJetpt:
-				secoundJetpt = firstJetpt
-				firstJetpt = Jet.pt()
-				firstJeteta = Jet.eta()
-			elif Jet.pt() >= secoundJetpt and Jet.pt() <= firstJetpt:
-				secoundJetpt = Jet.pt()
-	enumber=enumber+1
-	firstjetpt.fill(eventweight,firstJetpt)
-	secoundjetpt.fill(eventweight,secoundJetpt)
-	firstjeteta.fill(eventweight,firstJeteta)
-	njets.fill(eventweight,nJets)
+    # create handle outside of loop
+    # Handle and lable are pointing at the Branch you want
+    handle  = Handle ('std::vector<reco::GenJet>')
+    label = ("ak5GenJets")
+    infohandle = Handle ('<GenEventInfoProduct>')
 
-pt.write()
-phi.write()
-theta.write()
-energy.write()
-firstjetpt.write()
-secoundjetpt.write()
-firstjeteta.write()
-njets.write()
-"""
-        print 'Filling new particle histograms'
-#for pevent in events:
-        print "In pevent"
-        pevent = event
-	toppt = 0
-	tbarpt = 0
-	pevent.getByLabel (particlelabel, particlehandle)
-        print "event nr ", pevent
-	GenParticles = particlehandle.product()
-	for particle in GenParticles:
-                if abs(particle.pdgId())<= 6 or particle.pdgId() == 21:
-                #if particle.numberOfDaughters() == 0:
-                        print "particle nr ", particle
-                        print particle.pdgId()
-#		if particle.pdgId() == 6:
-#			toppt = particle.pt()
-#		if particle.pdgId() == -6:
-#			tbarpt = particle.pt
-		#Htpt.Fill(toppt)
-		#Htbarpt.Fill(tbarpt)
-		#if toppt != 0 and tbarpt != 0:
-			#Httbarpt.Fill(toppt + tbarpt)
-"""
+
+
+    #ROOT.gROOT.SetStyle('Plain') # white background
+    #Loop through all Events and Fill the Histograms
+    print 'Filling new jet histograms with ptcut: '+cutn+'GeV'
+    enumber = 0
+    print "handle_label",handle, label
+    for event in events:
+        event.getByLabel (label, handle)
+        GenJets = handle.product()
+        ievent = event
+        ievent.getByLabel ("generator", infohandle)
+        Infos = infohandle.product()
+        nJets = 0
+        firstJetpt = 0
+        secoundJetpt = 0
+        firstJeteta = 0
+        eventweight = Infos.weight()
+        for Jet in GenJets:
+            if Jet.pt() >= cut and abs(Jet.eta()) <= etacut:
+                #for JetConstituent in Jet.getJetConstituents():
+                #        print JetConstituent.pdgId(), " no of mothers ", JetConstituent.numberOfMothers()
+                nJets = nJets + 1
+                pt.fill(eventweight,Jet.pt())
+                phi.fill(eventweight,Jet.phi())
+                theta.fill(eventweight,Jet.theta())
+                energy.fill(eventweight,Jet.energy())
+                if Jet.pt() >= firstJetpt and Jet.pt() >= secoundJetpt:
+                    secoundJetpt = firstJetpt
+                    firstJetpt = Jet.pt()
+                    firstJeteta = Jet.eta()
+                elif Jet.pt() >= secoundJetpt and Jet.pt() <= firstJetpt:
+                    secoundJetpt = Jet.pt()
+                    enumber=enumber+1
+                    firstjetpt.fill(eventweight,firstJetpt)
+                    secoundjetpt.fill(eventweight,secoundJetpt)
+                    firstjeteta.fill(eventweight,firstJeteta)
+                    njets.fill(eventweight,nJets)
+    
+    #wirte all histograms in the output file. After they are wrote, they are getting deleted (s. write() method)
+    pt.write()
+    phi.write()
+    theta.write()
+    energy.write()
+    firstjetpt.write()
+    secoundjetpt.write()
+    firstjeteta.write()
+    njets.write()
+    #delete all variables, that are used again in the next loop
+    del handle
+    del label
+    del infohandle
+    del events
+    
+    
